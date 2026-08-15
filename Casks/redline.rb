e4bcdf1fd7d03e4fe11c4193f5b37e014f8dd715
Prashanth Goriparthi <github@goriparthi.com>
2026-08-14 18:15:58 -0600

    Version 0.1.4

--- Casks/redline.rb
+++ Casks/redline.rb
@@ -8,8 +8,8 @@
 #
 # Bump version and sha256 after each release; scripts/release.sh prints both.
 cask "redline" do
-  version "0.1.3"
-  sha256 "ed41b6ad88271720d00e3eba7b17f7cb212eacf88e8af9306f9d04724bf9693e"
+  version "0.1.4"
+  sha256 "0645a6063eb58816584c16f4a7409f4d8c892207500eedd3b6f5bfeb589759c8"
 
   url "https://github.com/goriparthi/redline/releases/download/v#{version}/Redline-#{version}.dmg"
   name "RedLine"
